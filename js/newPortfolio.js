7532fb4a072bb87e099525acc80286ac9aa498f1
Jbosco339 <benjaminadakole20@gmail.com>
2026-01-12 00:47:52 +0100

    color changes

--- js/newPortfolio.js
+++ js/newPortfolio.js
@@ -17,3 +17,51 @@ function animated(){
     requestAnimationFrame(animated);
 }
 animated();
+
+
+// AUTO WRITEUP BEGINS HERE
+
+const texts = [
+  "Turning ideas into interactive designs.",
+  "Focused on growth, impact, and clean code."
+];
+
+const speeds = 70;
+const pauseAfterAll = 2000;
+
+let textIndex = 0;
+let charIndex = 0;
+let output = "";
+
+function typeEffect() {
+  const el = document.getElementById("typeText");
+
+  if (charIndex < texts[textIndex].length) {
+    output += texts[textIndex].charAt(charIndex);
+    el.innerHTML = output;
+    charIndex++;
+    setTimeout(typeEffect, speeds);
+  } 
+  else {
+    output += "<br>";
+    el.innerHTML = output;
+
+    textIndex++;
+    charIndex = 0;
+
+    if (textIndex === texts.length) {
+      setTimeout(() => {
+        output = "";
+        textIndex = 0;
+        el.innerHTML = "";
+        typeEffect();
+      }, pauseAfterAll);
+    } else {
+      setTimeout(typeEffect, speeds);
+    }
+  }
+}
+
+typeEffect();
+
+
